3d443cf97b2ad3f2827aba4f3457d87e1f0aa8e5
Alvaro De Paula <114239272+AlvaroDePaula3@users.noreply.github.com>
2023-05-15 14:56:16 -0300

    Add files via upload

---
+++ Projeto de login/login e senha C#/login e senha C#/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace login_e_senha_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) { 
+            Console.WriteLine("Bem vindo ao cadastro do centro pokémon, digite seu nome de usuário: ");
+
+            string login = Console.ReadLine();
+
+            Console.WriteLine("Agora digite a sua senha");
+            string senha = "";
+
+            while (true)
+            {
+                ConsoleKeyInfo digito = Console.ReadKey(true);
+                if (digito.Key == ConsoleKey.Enter) 
+                    break;
+                else
+                {
+                    senha += digito.KeyChar;
+                }
+            }
+            if (login == "Ashe" && senha == "Pikachu")
+            {
+                Console.WriteLine("Usuário conectado com sucesso");
+                    break;
+            }
+            else
+            {
+                Console.WriteLine("Falha na conexão com o servidor, usuário desconhecido");
+                Console.WriteLine("Pressione qualquer tecla para tentar novamente");
+                Console.ReadKey();
+                Console.Clear();
+            }
+          }
+            Console.WriteLine("Pressione qualquer botão para sair");
+            Console.ReadKey();
+        }
+    }
+}
